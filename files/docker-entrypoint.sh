9c74d074a06a50fe40eb51e5452dfe88a4f0559c
Charles DELFLY <c.delfly@linkbynet.com>
2018-09-19 14:21:58 +0700

    Convert start script to entry script.

---
+++ files/docker-entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Optimise opcache.max_accelerated_files, if settings is too small
+nb_files=$(find ~www-data -type f -name '*.php' -print | wc -l)
+if [ ${nb_files} -gt ${PHP_OPCACHE_MAX_ACCELERATED_FILES} ]; then
+	echo "Change PHP_OPCACHE_MAX_ACCELERATED_FILES from ${PHP_OPCACHE_MAX_ACCELERATED_FILES} to ${nb_files}"
+	export PHP_OPCACHE_MAX_ACCELERATED_FILES=${nb_files}
+fi
+
+#su www-data -s /bin/bash -c 'mkdir -p runtime/logs'
+tail -F /app/runtime/logs/*.log /var/log/cron/*.log 2>/dev/null &
+
+# Loop on WAIT_FOR_IT_LIST
+if [ -n "${WAIT_FOR_IT_LIST}" ]; then
+	for hostport in $(echo "${WAIT_FOR_IT_LIST}" | sed -e 's/,/ /g'); do
+		/wait-for-it.sh -s -t 0 ${hostport}
+	done
+fi
+
+# Do database migration
+if [ -n "${YII_DB_MIGRATE}" -a "${YII_DB_MIGRATE}" = "true" ]; then
+	su www-data -s /bin/bash -c 'php yii migrate/up --interactive=0'
+fi
+
+# Do rbac migration (add/Update/delete rbac permissions/roles)
+if [ -n "${YII_RBAC_MIGRATE}" -a "${YII_RBAC_MIGRATE}" = "true" ]; then
+    su www-data -s /bin/bash -c 'php yii rbac/load rbac.yml'
+fi
+
+if [ -n "${1}" ]; then
+	echo "Command line: ${@}"
+else
+	echo "No command line running Apache HTTPD server"
+fi
+
+if [ "${1}" = "yii" ]; then
+	su www-data -s /bin/bash -c "php ${@}"
+elif [ "${1}" = "cron" ]; then
+	# Add current environment to /etc/environment so it will be available for job in cron
+	env > /etc/environment
+	exec /usr/sbin/cron -f
+else
+	exec "apache2-foreground"
+fi
